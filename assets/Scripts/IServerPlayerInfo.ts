af09ec5bbc5c3755d3b7e74f8499310631204a78
gerwang <gerwang@outlook.com>
2018-07-18 10:52:03 +0800

    remove duplicate playerID

--- assets/Scripts/IServerPlayerInfo.ts
+++ assets/Scripts/IServerPlayerInfo.ts
@@ -2,7 +2,6 @@ import { GameAI } from './GameAI';
 import { IPlayerInfo } from './IPlayerInfo';
 
 export interface IServerPlayerInfo extends IPlayerInfo {
-    playerID: number;
     isAI: boolean;
     aiInstance: GameAI;
     nextDirection: number;
