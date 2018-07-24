d4f3d97c459acbe56ceab8addf7f2577468f8e9e
Zhang Yang <dimlight1998@gmail.com>
2018-07-24 16:44:07 +0800

    Possible fix.

--- assets/Scripts/CameraController.ts
+++ assets/Scripts/CameraController.ts
@@ -3,14 +3,20 @@ const { ccclass } = cc._decorator;
 @ccclass
 export default class CameraController extends cc.Component {
     private follower: cc.Node = null;
+    private static updateFreqInit = 1;
+    private updateCount = 0;
 
     public setFollower(follower: cc.Node): void {
         this.follower = follower;
     }
 
     update(): void {
-        if (this.follower !== null) {
+        if (this.updateCount !== 0) {
+            this.updateCount--;
+            return;
+        } else if (this.follower !== null) {
             this.node.position = this.follower.position;
+            this.updateCount = CameraController.updateFreqInit;
         }
     }
 }
